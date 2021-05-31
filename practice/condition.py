7cba9b9bcc3f97e40859fa9caacc33e24a2cd1b1
ye0219 <85010170+ye0219@users.noreply.github.com>
2021-05-31 16:41:28 +0800

    Create condition.py
    practice how to use if elif else

---
+++ practice/condition.py
@@ -0,0 +1,12 @@
+X=input("請輸入數字:5,2,0:")
+X=int(X)
+if X==5:
+    print("I")
+elif X==0:
+    print("YOU")
+elif X==2:
+    print("LOVE")
+elif X==520:
+    print("I" "LOVE" "YOU" )
+else:
+    print("THANK YOU")
